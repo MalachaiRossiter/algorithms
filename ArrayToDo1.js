1c519784771769111c74e21beee186ab0f57737d
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-02-07 19:40:28 -0500

    finished arrayToDo1

--- ArrayToDo1.js
+++ ArrayToDo1.js
@@ -11,7 +11,32 @@ function pushFront(arr, val) {
     for(i=0; i < temp.length; i++){
         arr[i+1] = temp[i];
     }
-    console.log(arr);
+    return arr;
 }
 
-pushFront(array1, value);
+// console.log(pushFront(array1, value));
+
+function popFront(arr) {
+    let temp = arr[0];
+
+    for (let i=0; i < arr.length; i++){
+        arr[i] = arr[i + 1];
+
+        arr.length = arr.length -1;
+        console.log(arr);
+        return temp;
+    }
+}
+
+// console.log(pushFront(array1));
+
+function insertAt(arr, index, val) {
+    for (i = arr.length; i >= index; i--) {
+        arr[i] = arr[i -1]
+    }
+    arr[index] = val;
+
+    return arr
+}
+
+console.log(insertAt(array1, 4, value));
