e6dbce1d33542135e0a4751f2a5a05f5d1fef32f
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-03-05 23:48:56 -0500

    kthMissingNumber-leetcode

---
+++ kthMissingNumber.js
@@ -0,0 +1,14 @@
+let array =  [2,3,4,7,11];
+let k = 5;
+
+findkth = function(arr, k) {
+    //itterate through array in acending order until we find a missing number,
+    //note theres a missing number until noted number reatches k value and return number
+    let bigArr = [];
+    let missing = [];
+    for (i=1; i <= arr[arr.length-1]; i++){
+        bigArr.push(i);
+    }
+}
+
+findkth(array, k);
