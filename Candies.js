f11ea84b1a9bf8f982dfe3843f0306538f7dcca7
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-04-16 23:42:33 -0400

    work on candies algorithem

---
+++ Candies.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    for (i = 0; i <= candies.length-1; i++){
+        candies[i] += extraCandies;
+        for (i = 0; i <= candies.length-1; i++){
+            if (candi)
+        }
+    }
+};
