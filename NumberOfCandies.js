f53f85b453adfa4eaabfddafec0297ceb1235503
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-04-19 22:41:41 -0400

    finished this algorithems puzzle

---
+++ NumberOfCandies.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minimumCost = function(cost) {
+    cost.sort((a, b) => b - a);
+
+    let sum = 0;
+    for (let i = 0; i < cost.length; i += 3) {
+        sum += cost[i] + (cost?.[i + 1] ?? 0);
+    }
+
+    return sum;
+};
